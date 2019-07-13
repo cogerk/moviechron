b3244053ebef3c3c8b3ee2946b9cd6819e26e138
Kathryn Cogert <cogerk@gmail.com>
2019-07-13 16:58:35 -0700

    Updated views.py

---
+++ game.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import warnings
+import random
+
+easy_df = pd.read_csv('data/selected_films_easy.tsv', sep='\t', index_col=0)
+med_df = pd.read_csv('data/selected_films_easy.tsv', sep='\t', index_col=0)
+hard_df = pd.read_csv('data/selected_films_easy.tsv', sep='\t', index_col=0)
+imp_df = pd.read_csv('data/selected_films_easy.tsv', sep='\t', index_col=0)
+# TODO: Replace above tsv files with respective difficulty files
+# TODO: User can choose difficulty and deck size
+# TODO: points system, multiplayer, save game
+# TODO: Clean up javscript
+# TODO: Clean up database
+# TODO: Messages and game complete 
+
+class Game:
+    """
+    An instance of a game w/ unqiue game id
+    """
+    def __init__(self, game_id):
+        self.game_id = game_id
+        # Game difficult defined by game id
+        self.game_diff = game_id.split('_')[0]
+        # deck size defined by game id
+        self.deck_size = int(game_id.split('_')[1]) 
+        # Cards in deck defined by game id
+        self.deck_id = int(game_id.split('_')[2])
+        # Get deck using random state & game difficulty from game id
+        if self.game_diff == 'E':
+            self.deck = easy_df.sample(self.deck_size, random_state=self.deck_id).reset_index(drop=True)
+        elif self.game_diff == 'M':
+            self.deck = med_df.sample(self.deck_size, random_state=self.deck_id).reset_index(drop=True)
+        elif self.game_diff == 'H':
+            self.deck = hard_df.sample(self.deck_size, random_state=self.deck_id).reset_index(drop=True)
+        elif self.game_diff == 'I':
+            self.deck = imp_df.sample(self.deck_size, random_state=self.deck_id).reset_index(drop=True)
+        else:
+            self.deck = easy_df.sample(self.deck_size, random_state=self.deck_id).reset_index(drop=True)
+            warnings.warn('Invalid difficulty, using easy.', SyntaxWarning)
+        # Game status defined by game id
+        status = game_id.split('_')[3] 
+        self.deck['status']=['T'] + list(status) + ['D'] * (self.deck_size-len(status)-1)
+        self.deck_loc = len(status)+1 
+        # 'T' means the card is in the timeline
+        self.timeline = self.deck[self.deck['status'] == 'T'].reset_index(drop=True)
+        # 'W' means the card is in the wings
+        self.wings = self.deck[self.deck['status'] == 'W'].reset_index(drop=True)
+
+        
+        
+    def make_move(self, wing_select, location_guess):
+        """
+        Given user selection from wings and guessed location, determine if guess is correct
+        """
+        # Get date of selected movie from the wings
+        to_place = self.wings.iloc[int(wing_select)]
+        true_date = to_place['startYear']
+
+        
+        # Determine acceptable date range from location guess
+        if location_guess <= 0:
+            compare_date_below = -float('inf')
+            compare_date_above = int(self.timeline.iloc[0]['startYear'])
+            compare_above = self.timeline.iloc[0]
+            print('After: '+str(compare_date_below))
+            print('Before: '+str(compare_date_above))
+            print(compare_above)
+        elif location_guess > (len(self.timeline)-1):
+            compare_below = self.timeline.iloc[len(self.timeline)-1]
+            compare_date_below = int(self.timeline.iloc[len(self.timeline)-1]['startYear'])
+            compare_date_above = float('inf')
+            print('After: '+str(compare_date_below))
+            print(compare_below)
+            print('Before: '+str(compare_date_above))
+        else:
+            compare_below = self.timeline.iloc[location_guess-1]
+            compare_above = self.timeline.iloc[location_guess]
+            compare_date_below = int(self.timeline.iloc[location_guess-1]['startYear'])
+            compare_date_above = int(self.timeline.iloc[location_guess]['startYear'])
+            print('After: '+str(compare_date_below))
+            print(compare_below)
+            print('Before: ' +str(compare_date_above))
+            print(compare_above)
+        # Determine if guess is correct
+        if compare_date_below <= true_date <= compare_date_above:
+            messages = ['Great Work!']
+            # Update Timeline & deck status if guess correct
+            self.timeline = self.timeline.append(to_place)
+            # 'T' means the card is in the timeline
+            self.deck.loc[to_place.tconst == self.deck.tconst, 'status'] = 'T'
+            """
+            Game status update
+            """ 
+            # If same year, compare on release date for fun
+            if compare_date_below == true_date:
+                """Code for close ones"""
+                if compare_below['releaseDate'] < to_place['releaseDate']:
+                    messages = messages + ['A Close one, and you nailed it!']
+                    """Code for close calls here"""
+                else:
+                    messages = messages + ['close enough!']
+            elif compare_date_above == true_date:
+                if to_place['releaseDate'] < compare_above['releaseDate']:
+                    messages = messages + ['A Close one, and you nailed it!']
+                    """Code for close calls here"""
+                else:
+                    messages = messages + ['close enough!']
+        else:
+            # Update deck status if incorrect
+            messages = ['Nope.', to_place['primaryTitle']+' came out in '+str(int(true_date))]
+            # 'I' means the card is discarded because of incorrect guess
+            self.deck.loc[to_place.tconst == self.deck.tconst, 'status'] = 'I'
+            """
+            Losing points code here
+            """
+        # Draw a new card
+        self.wings.drop(int(wing_select), inplace=True)
+        if self.deck_loc < (self.deck_size-1):
+            self.deck.loc[self.deck_loc, 'status'] = 'W'
+            self.deck_loc = self.deck_loc + 1
+            self.wings = self.deck[self.deck['status'] == 'W']
+            self.wings = self.wings.reset_index(drop=True)
+
+        else:
+            self.wings.reset_index(drop=True, inplace=True)
+        
+        # Update status string & sort timeline
+        status_string = ''.join(list(self.deck.status))
+        self.game_id = self.game_diff + '_' + format(self.deck_size, '03d') + '_' + str(self.deck_id) + '_' + status_string[1:].rstrip('D')
+        self.timeline = self.timeline.sort_values(by='releaseDate').reset_index(drop=True)
+        return self, messages
+    
+    def deal(game_diff, deck_size):
+        """
+        Given a deck ID and game status string, define all game attributes
+        """
+        deck_size = deck_size 
+        # Unqiue deck ID used to draw cards
+        deck_id = random.randint(100000000,1000000000)
+        # Game status contains deck size, deck ID and game status string
+        game_id = game_diff + '_' + format(deck_size, '03d') + '_' + str(deck_id) +'_WWW'
+        self = Game(game_id)
+        return self
+        
+    
--- views.py
+++ views.py
@@ -1,28 +1,32 @@
-from flask import Flask
-from flask import render_template
-import pandas as pd
-
-films_df = pd.read_csv('data/selected_films_easy.tsv', sep='\t', index_col=0)
-n = 14
-deck = films_df.sample(n=10)
-
-
+from flask import Flask, render_template, request, redirect, url_for, json, jsonify
+from game import Game
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    # Select first card
-    deck_loc = 0
-    timeline = deck.iloc[deck_loc:deck_loc+1].reset_index()
-    deck_loc = deck_loc +1    
-    
-    # Init Wings
-    wings = deck.iloc[deck_loc:deck_loc+3].reset_index()
-    deck_loc = deck_loc +3
-    
-    return render_template("game.html", 
-                           timeline= timeline,
-                           wings = wings
-                          )
+@app.route('/',  methods=['GET', 'POST'])
+def deal():
+    """Start the game"""
+    game= Game.deal('E', 10)
+    # Send new game to template
+    return render_template("game.html", game=game, messages='Welcome! Select a card from the wings and place on the timeline')
 
+
+@app.route('/send_guess', methods=['GET', 'POST'])
+def send_guess():
+    """
+    Runs when user makes a guess
+    """
+    # Get info from web app
+    game_id = request.json['game_id'] # Unique id for the deck (int)
+    guess_id = int(request.json['guess_id'].split('_')[1]) # index of movie in the wings user selected
+    location_guess = int(request.json['location_id'].split('_')[1]) # Location on timeline user selected
+    
+    print('Location on timeline:')
+    print(location_guess)
+    
+    # Pull up the game being played & make the move
+    game = Game(game_id)
+    game, messages = game.make_move(guess_id, location_guess)
+    print(messages)
+    var = render_template("body.html", game=game, messages=messages)
+    return var
